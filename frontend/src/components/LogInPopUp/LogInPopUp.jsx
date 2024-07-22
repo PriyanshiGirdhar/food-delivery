800ed31b6eca57d4dde4c736274c348752dfaba8
Priyanshi Girdhar <girdharpriyanshi2@gmail.com>
2024-07-22 23:37:12 +0530

    update

--- frontend/src/components/LogInPopUp/LogInPopUp.jsx
+++ frontend/src/components/LogInPopUp/LogInPopUp.jsx
@@ -1,35 +1,64 @@
-import React, { useState } from 'react'
-import './LogInPopUp.css'
-const LogInPopUp = ({setShowLogin}) => {
-    const[currState,setCurrState]=useState("Login")
+import React, { useState } from "react";
+import "./LogInPopUp.css";
+const LogInPopUp = ({ setShowLogin }) => {
+  const [currState, setCurrState] = useState("Login");
+
+  const[data,setData]=useState({
+name:"",
+email:"",
+password:""
+
+  })
+
+  const onChangeHandler=(event)=>{
+const name=event.target.name;
+const value=event.target.value;
+setData(data=>({...data,[name]:value}))
+  }
+ 
   return (
-    <div className='login-popup'>
-    <form  className="login-popup-container">
+    <div className="login-popup">
+      <form className="login-popup-container">
         <div className="login-popup-title">
-            <h2>{currState}</h2>
-            <img  onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt=''/>
+          <h2>{currState}</h2>
+
+          <img
+            onClick={() => setShowLogin(false)}
+            src={assets.cross_icon}
+            alt=""
+          />
         </div>
-        <div className='login-popup-inputs'>
-      
-            {currState==="Login"?<></>: <input type='text' placeholder='Your Name'  required/>}
-            <input type='email' placeholder='Your email' required/>
-            <input type='password' placeholder='Password' required/>
+
+        <div className="login-popup-inputs">
+          {currState === "Login" ? (
+            <></>
+          ) : (
+            <input name="name" onChange={onChangeHandler} value={data.name}type="text" placeholder="Your Name" required />
+          )}
+          <input name="email"  onChange={onChangeHandler} value={data.email}type="email" placeholder="Your email" required />
+          <input name="password" onChange={onChangeHandler} value={data.password}type="password" placeholder="Password" required />
         </div>
-        <button>{currState==='Sign Up'?'Create account':'Login'}</button>
+        <button>{currState === "Sign Up" ? "Create account" : "Login"}</button>
         <div className="login-popup-condition">
-            <input type='checkbox' required/>
-            <p>By continuing ,I agree to the term of use and privacy policy</p>
+          <input type="checkbox" required />
+          <p>By continuing ,I agree to the term of use and privacy policy</p>
         </div>
-        {
-            currState==="Login"?
-            <p>Create a new account?<span onClick={()=>setCurrState("Sign Up")}>Click here</span></p>
-            :<p>Already hav an account?<span onClick={()=>setCurrState("Login")}>Login here</span></p>
-        }
-        
-    </form>
+        {currState === "Login" ? (
+          <p>
+            Create a new account?
+            <span onClick={() => setCurrState("Sign Up")}>Click here</span>
+          </p>
+        ) : (
+          <p>
+            Already hav an account?
+            <span onClick={() => setCurrState("Login")}>Login here</span>
+          </p>
+        )}
+      </form>
     </div>
-  )
-}
-import './LogInPopUp.css'
-import { assets } from '../../assets/assets'
-export default LogInPopUp
+  );
+};
+import "./LogInPopUp.css";
+import { assets } from "../../assets/assets";
+import { log } from "console";
+export default LogInPopUp;
